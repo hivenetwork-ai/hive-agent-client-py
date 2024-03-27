36fe3184f8be84a3464553eab7d36884f87f7059
Tomisin Jenrola <tomisinjenrola@gmail.com>
2024-03-27 17:53:28 -0400

    feat: convert to installable package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hive-agent-client',
+    version='0.0.1',
+    packages=find_packages(include=['hive_agent_client', 'hive_agent_client.*']),
+    install_requires=[
+        'requests>=2.31.0'
+    ],
+    description='A client library for sending messages to a Hive Agent',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
